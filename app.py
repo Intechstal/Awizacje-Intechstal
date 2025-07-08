59af764e13d33c44c2be990db12537fa0cee0547
Intechstal <s.ksawery@intechstal.pl>
2025-07-08 18:30:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-efrom flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 import sqlite3
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
