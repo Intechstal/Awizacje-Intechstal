b3e9104a82090b8b55045ce14754f480cacb655e
Intechstal <s.ksawery@intechstal.pl>
2026-04-27 13:00:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def create_users():
     conn = sqlite3.connect("awizacje.db")
     c = conn.cursor()
 
-    users = [("admin1","1234"),("admin2","1234"),("admin3","1234"),("admin4","1234")]
+    users = [("SK","1234"),("BL","1234"),("JU","1234"),("KJ","1234")]
 
     for u in users:
         try:
